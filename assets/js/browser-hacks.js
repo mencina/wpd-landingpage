0e554cbbda3e42982105a9a70aeb0006ff2b3bfa
Mario Encina <mencina@Marios-MacBook-Air.local>
2014-08-14 16:43:58 -0400

    retina images

--- assets/js/browser-hacks.js
+++ assets/js/browser-hacks.js
@@ -1,8 +1,8 @@
 // Safari
 $(document).ready(function() {
   if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {
-    $(".facebook-share").css("padding", "12px 28px");
-    $(".twitter-share").css("padding", "12px 37px");
+    $(".facebook-share").css("padding", "12px 25.5px");
+    $(".twitter-share").css("padding", "12px 35px");
     $(".facebook-share, .twitter-share, .email-field, .submit-button").addClass("no-smoothing");
   }
 });
