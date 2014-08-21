1f90a39046700dcf7d944e250c576666d8e26957
Mario Encina <mencina@Marios-MacBook-Air.local>
2014-08-21 14:53:21 -0400

    fixed safari text issues

--- assets/js/browser-hacks.js
+++ assets/js/browser-hacks.js
@@ -1,6 +1,5 @@
 // Safari
 $(document).ready(function() {
   if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {
-    $(".facebook-share, .twitter-share, .email-field, .submit-button").addClass("no-smoothing");
   }
 });
